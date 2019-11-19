a820aba424df7ddc4fccddfeb06729e75fc018a1
Jes <ilamtibr@gmail.com>
2019-11-19 10:57:44 +0800

    Wait api

---
+++ BathHouse/BathHouseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BathHouseViewController.swift
+//  BathHouse
+//
+//  Created by Jes Yang on 2019/11/19.
+//  Copyright © 2019 Jes Yang. All rights reserved.
+//
+
+import UIKit
+
+class BathHouseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension BathHouseViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
